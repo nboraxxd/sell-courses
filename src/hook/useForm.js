b364c59bbbd247243d1207ac771e527716cadfba
bora <nhatbaoxxd@gmail.com>
2023-09-01 11:53:47 +0700

    Refactor contact page with useForm

---
+++ src/hook/useForm.js
@@ -0,0 +1,34 @@
+import { validate } from '@/utils/validate'
+import { useState } from 'react'
+
+/**
+ *
+ * @param {*} rules
+ * @returns values, errors, register
+ */
+export default function useForm(rules) {
+  const [values, setValues] = useState({})
+  const [errors, setErrors] = useState({})
+
+  function register(name) {
+    return {
+      value: values[name] || '',
+      error: errors[name] || '',
+      onChange: (ev) => setValues((form) => ({ ...form, [name]: ev.target.value })),
+    }
+  }
+
+  function isValid() {
+    const errorObject = validate(rules, values)
+    setErrors(errorObject)
+
+    return Object.keys(errorObject).length === 0
+  }
+
+  return {
+    values,
+    errors,
+    register,
+    isValid,
+  }
+}
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,50 +1,35 @@
 import { TextField } from '@/components/TextField'
-import { regexp, required, validate } from '@/utils/validate'
-import { useState } from 'react'
+import useForm from '@/hook/useForm'
+import { regexp, required } from '@/utils/validate'
 
 export default function Contact() {
-  const [form, setForm] = useState({})
-  const [error, setError] = useState({})
-
-  function register(name) {
-    return {
-      value: form[name] || '',
-      error: error[name] || '',
-      onChange: (ev) => setForm((form) => ({ ...form, [name]: ev.target.value })),
-    }
-  }
+  const { values, errors, register, isValid } = useForm({
+    name: [required('Vui lòng nhập họ và tên của bạn')],
+    email: [required('Vui lòng nhập email của bạn'), regexp('email', 'Email chưa đúng định dạng')],
+    phone: [required('Vui lòng nhập số điện thoại của bạn', regexp('phone', 'Số điện thoại chưa đúng định dạng'))],
+    website: [
+      regexp(
+        /[(http(s)?)://(www.)?a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/,
+        'Đường dẫn website chưa đúng định dạng',
+      ),
+    ],
+    title: [required('Vui lòng nhập tiêu đề liên hệ')],
+    content: [required('Vui lòng nhập nội dung liên hệ')],
+  })
 
   function handleOnSubmit(ev) {
     ev.preventDefault()
 
-    const errorObject = validate(
-      {
-        name: [required('Vui lòng nhập họ và tên của bạn')],
-        email: [required('Vui lòng nhập email của bạn'), regexp('email', 'Email chưa đúng định dạng')],
-        phone: [required('Vui lòng nhập số điện thoại của bạn', regexp('phone', 'Số điện thoại chưa đúng định dạng'))],
-        website: [
-          regexp(
-            /[(http(s)?)://(www.)?a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/,
-            'Đường dẫn website chưa đúng định dạng',
-          ),
-        ],
-        title: [required('Vui lòng nhập tiêu đề liên hệ')],
-        content: [required('Vui lòng nhập nội dung liên hệ')],
-      },
-      form,
-    )
-
-    setError(errorObject)
-    if (Object.keys(errorObject).length === 0) {
-      const trimmedForm = { ...form }
-      for (const key in trimmedForm) {
-        if (typeof trimmedForm[key] === 'string') {
-          trimmedForm[key] = trimmedForm[key].trim()
+    if (isValid() === true) {
+      const trimmedValues = { ...values }
+      for (const key in trimmedValues) {
+        if (typeof trimmedValues[key] === 'string') {
+          trimmedValues[key] = trimmedValues[key].trim()
         }
       }
-      console.log('Validate success', trimmedForm)
+      console.log('Validate success', trimmedValues)
     } else {
-      console.log('Validate error', errorObject)
+      console.log('Validate error', errors)
     }
   }
 
@@ -52,7 +37,6 @@ export default function Contact() {
     <main id="main">
       <div className="register-course">
         <section className="section-1 wrap container">
-          {/* <div class="main-sub-title">liên hệ</div> */}
           <h2 className="main-title">HỢP TÁC CÙNG Spacedev</h2>
           <p className="top-des">
             Đừng ngần ngại liên hệ với <strong>Spacedev</strong> để cùng nhau tạo ra những sản phẩm giá trị, cũng như
@@ -79,17 +63,6 @@ export default function Contact() {
             <button className="btn main rect">đăng ký</button>
           </form>
         </section>
-        {/* <div class="register-success">
-          <div class="contain">
-              <div class="main-title">đăng ký thành công</div>
-              <p>
-                  <strong>Chào mừng Vương Đặng đã trở thành thành viên mới của Spacedev Team.</strong> <br>
-                  Cảm ơn bạn đã đăng ký khóa học tại <strong>Spacedev</strong>, chúng tôi sẽ chủ động liên lạc với bạn thông qua facebook
-                  hoặc số điện thoại của bạn.
-              </p>
-          </div>
-          <a href="/" class="btn main rect">về trang chủ</a>
-      </div> */}
       </div>
     </main>
   )
