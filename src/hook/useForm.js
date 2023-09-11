7dd968937b689fc0803e7d243b48846904a84cd2
bora <nhatbaoxxd@gmail.com>
2023-09-12 03:59:04 +0700

    Handles validation every time the input is changed

--- src/hook/useForm.js
+++ src/hook/useForm.js
@@ -14,7 +14,15 @@ export default function useForm(rules, initialValue = {}) {
     return {
       error: errors[name],
       value: values[name] || '',
-      onChange: (ev) => setValues((values) => ({ ...values, [name]: ev.target.value })),
+      onChange: (ev) => {
+        let _values = { ...values, [name]: ev.target.value }
+        if (rules[name]) {
+          const error = validate({ [name]: rules[name] }, _values)
+          setErrors((prev) => ({ ...prev, [name]: error[name] || '' }))
+        }
+
+        setValues((values) => ({ ...values, [name]: ev.target.value }))
+      },
     }
   }
 
